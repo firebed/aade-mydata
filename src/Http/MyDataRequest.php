304d29fb8aa51d8f8f6c6710ce764adfbe54e23e
okangr <okan.giritli@gmail.com>
2023-02-01 03:04:03 +0200

    Added ability to change the GuzzleHttp\Client verification.

--- src/Http/MyDataRequest.php
+++ src/Http/MyDataRequest.php
@@ -12,9 +12,10 @@ use GuzzleHttp\Exception\GuzzleException;
 
 abstract class MyDataRequest
 {
-    private static ?string $user_id          = null;
-    private static ?string $subscription_key = null;
-    private static ?string $env              = null;
+    private static ?string     $user_id          = null;
+    private static ?string     $subscription_key = null;
+    private static ?string     $env              = null;
+    private static bool|string $verify_client    = true;
 
     private static mixed $mockResponse;
 
@@ -43,6 +44,39 @@ abstract class MyDataRequest
     {
         self::$user_id = $user_id;
         self::$subscription_key = $subscription_key;
+    }
+
+    /**
+     * <ul>Describes the SSL certificate verification behavior of a request.
+     *
+     * <li>Set to true to enable SSL certificate verification and use the default CA bundle provided by operating system.</li>
+     * <li>Set to false to disable certificate verification (this is insecure!).</li>
+     * <li>Set to a string to provide the path to a CA bundle to enable verification using a custom certificate.</li>
+     * </ul>
+     *
+     * <pre>
+     * // Use the system's CA bundle (this is the default setting)
+     * MyDataRequest::verifyClient(true);
+     *
+     * // Use a custom SSL certificate on disk.
+     * MyDataRequest::verifyClient('/path/to/cert.pem');
+     *
+     * // Disable validation entirely (don't do this!).
+     * MyDataRequest::verifyClient(false);
+     * </pre>
+     * 
+     * <br>
+     * <p>If you do not need a specific certificate bundle, then Mozilla provides a commonly used CA bundle which can be downloaded <a href="https://curl.haxx.se/ca/cacert.pem">here</a>
+     * (provided by the maintainer of cURL). Once you have a CA bundle available on disk, you can set the "openssl.cafile" PHP ini
+     * setting to point to the path to the file, allowing you to omit the "verify" request option.
+     * Much more detail on SSL certificates can be found on the <a href="http://curl.haxx.se/docs/sslcerts.html">cURL website</a>.</p>
+     *
+     * @param bool|string $verify
+     * @return void
+     */
+    public static function verifyClient(bool|string $verify = true): void
+    {
+        self::$verify_client = $verify;
     }
 
     public static function setEnvironment($env): void
@@ -120,6 +154,7 @@ abstract class MyDataRequest
                 'Ocp-Apim-Subscription-Key' => self::$subscription_key,
                 'Content-Type'              => "text/xml"
             ],
+            'verify'  => self::$verify_client
         ]);
     }
 
